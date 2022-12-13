4ddb485ad361b2b2d6cd2220331d4f4d40bf432d
suraj beragu <x21117951@student.ncirl.ie>
2022-12-13 19:13:59 +0000

    first commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/surajberagu/Documents/MSc Cloud Computing Sem 3/Research Project /Deploy/Heart-Care-Django/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
